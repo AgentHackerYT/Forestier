ae2c05c9154bd801b7763056fb99a2ae05dde03f
RadonDevelopment <95672518+RadonDevelopment@users.noreply.github.com>
2022-01-02 19:43:07 +0530

    Update oo.js

--- commands/info/fun/oo.js
+++ commands/info/fun/oo.js
@@ -1,1 +1,18 @@
+const simplydjs = require("simply-djs");
 
+
+
+
+
+
+
+simplydjs.rps(message, {
+  embedColor: "#075FFF", // default: #075FFF
+  timeoutEmbedColor: "#c90000", // default: #c90000
+  drawEmbedColor: "#075FFF", // default: #075FFF
+  winEmbedColor: "#06bd00", // default: #06bd00
+  embedFooter: "Make sure you Win ;)",
+  rockColor: "colors", // default: SECONDARY
+  paperColor: "colors", // default: SECONDARY
+  scissorsColor: "colors" // default: SECONDARY
+});
